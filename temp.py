e0305716e2866dcf9eb4884813611c1d875469e3
fupeiyin <98298182+fupeiyin@users.noreply.github.com>
2023-09-06 16:13:25 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+H=float(input("請輸入您的身高(公尺)"))
+W=float(input("請輸入您的體重(公斤)"))
+BMI=W/(H**2)
+print(f"您的BMI為{BMI:2f}")
+
+x
